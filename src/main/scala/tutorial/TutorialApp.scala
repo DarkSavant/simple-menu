b586cb3d23d8db701916c6073df1371b26828085
curtis cooley <curtis@industriallogic.com>
2016-11-07 07:30:02 -0800

    Initial commit

---
+++ src/main/scala/tutorial/TutorialApp.scala
@@ -0,0 +1,20 @@
+package tutorial.webapp
+
+import org.scalajs.jquery.jQuery
+
+import scala.scalajs.js.JSApp
+
+object TutorialApp extends JSApp {
+  def main(): Unit = {
+    jQuery(setupUI _)
+  }
+
+  def addClickedMessage(): Unit = {
+    jQuery("body").append("<p>[message]</p>")
+  }
+
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(addClickedMessage _)
+    jQuery("body").append("<p>Hello World</p>")
+  }
+}
